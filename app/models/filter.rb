f0656fe9966f6745cdf8ee2deb88971b05c64303
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-08 10:23:33 -0500

    Add basic validation

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,3 +1,9 @@
 class Filter < ApplicationRecord
   belongs_to :visualization
+  validates :variable_name, presence: true
+  validates :filter_type, presence: true
+  validates :value1, presence: true
+  # validates :value2, presence: true, unless: -> { value1.blank? }
+  # TODO above
+  
 end
