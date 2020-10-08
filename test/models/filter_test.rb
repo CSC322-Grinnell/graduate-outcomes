bec0a6fd3e64355a1ada95ac0274526445b932d3
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-08 10:24:35 -0500

    Add filter creation test

--- test/models/filter_test.rb
+++ test/models/filter_test.rb
@@ -4,4 +4,14 @@ class FilterTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @visualization = Visualization.new()
+    @filter = @visualization.filters.new(variable_name:"major1", filter_type:"equals", value1:"Computer Science",  value2:"Sociology")
+  end
+
+  test "should create filter" do
+    assert @filter.valid?
+  end
+
 end
