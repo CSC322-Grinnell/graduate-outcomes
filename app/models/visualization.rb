5183a49075f042e1f5b97f4faed9007163c5fcbd
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-18 18:27:53 -0500

    Fix error on has_many

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -1,9 +1,9 @@
 class Visualization < ApplicationRecord
-  has_many :filters, :variables, inverse_of: :visualization
+  has_many :variables, inverse_of: :visualization
+  has_many :filters, inverse_of: :visualization
+  accepts_nested_attributes_for :variables, :filter
   validates :chart_title, presence: true
   validates :x_axis_title, presence: true
   validates :y_axis_title, presence: true
   validates :chart_type, presence: true
-  has_many :filters, inverse_of: :visualization
-  accepts_nested_attributes_for :variables, :filters
 end
