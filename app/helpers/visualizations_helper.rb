9250f55dc907edc5096684816f1ba44521fc8b15
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-18 01:33:42 -0500

    Make visualization helpers work using visualization id

--- app/helpers/visualizations_helper.rb
+++ app/helpers/visualizations_helper.rb
@@ -67,8 +67,9 @@ module VisualizationsHelper
         ]
     end
 
-    def get_chart(visualization)
+    def get_chart_info(visualization_id)
         data = Student.all;
+        visualization = Visualization.find(visualization_id)
         chart_type = visualization.chart_type
 
         # Repeatedly apply each filter on the dataset
@@ -77,70 +78,85 @@ module VisualizationsHelper
         end
 
         visualization.variables.each do |variable|
-            data = set_variable(data, variable)
+            data = set_variable(data, variable, chart_type)
         end
 
+        chart_info = Hash.new
+        chart_info[:data] = data
+        chart_info[:chart_type] = chart_type
         # Render visualization partial. This partial should have logic to determine which chart to graph
-        render partial: "visualization", locals: {data: data, chart_type: chart_type}
+        return chart_info
     end
+
 
 
     # Applies a single filter on the dataset
     def apply_filter(data, filter_model)
 
-        filter_hash = Hash.new
+        filter_type = filter_model.filter_type
         variable_name = filter_model.variable_name;
         value1 = filter_model.value1
         value2 = filter_model.value2
 
-        case filter
-        when 'equals_to'
-            filter_hash[variable_name] = value1
-            data = data.where(filter_hash)
+        case filter_type
+            when 'equals_to'
+                filter_hash = Hash.new
+                filter_hash[variable_name] = value1
+                data = data.where(filter_hash)
 
-        when 'greater_than'
-            data = data.where("#{variable_name} > ?", variable_name, value1)
+            when 'greater_than'
+                data = data.where("#{variable_name} > ?", variable_name, value1)
 
-        when 'greater_than_or_equal'
-            data = data.where("#{variable_name} >= ?", value1)
+            when 'greater_than_or_equal'
+                data = data.where("#{variable_name} >= ?", value1)
 
-        when 'less_than'
-            data = data.where("#{variable_name} > ?", value1)
+            when 'less_than'
+                data = data.where("#{variable_name} > ?", value1)
 
-        when 'less_than_or_equal'
-            data = data.where("#{variable_name} >= ?", value1)
+            when 'less_than_or_equal'
+                data = data.where("#{variable_name} >= ?", value1)
 
-        when 'from_to'
-            data = data.where("#{variable_name} >= ? AND #{variable_name} <= ?", value1, value2)
+            when 'from_to'
+                data = data.where("#{variable_name} >= ? AND #{variable_name} <= ?", value1, value2)
 
-        else
-            data
+            else
+                data
         end
     end
 
 
-    def set_variable(data, variable_model)
+    def set_variable(data, variable_model, chart_type)
         
         role = variable_model.role
         variable_name = variable_model.name
-        calculation = variable_model.calculation
+        #TO DO: add more variable types (Independent, Dependent)
+        if variable_name.length == 0
+            return data
+        end
 
         case role
-        when 'group'
-            data = data.group(variable_name)
-        else
-            data
+            when 'group'
+                data = data.group(variable_name)
+                data = summarize(data, 'count')
+            when 'independent'
+                data = data.count
+            when 'dependent'
+                data = data.count
+            else
+                data.count
         end
     end
 
 
-    def summarize(data, summarization_method)
+    def summarize(data, summarization_method, variable=nil)
         case summarization_method
-        when 'sum'
-            data = data.sum()
-        when 'count'
-            data = data.count()
-        end
+            when 'sum'
+                data = data.sum(variable)
+            when 'max'
+                data = data.maximum(variable)
+            when 'count'
+                data = data.count(variable)
+            end
     end
 
 
