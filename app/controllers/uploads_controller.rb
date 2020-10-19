e1d0578211c9c201bc422e125db97cbd8a80f308
Rexford Essilfie <rexfordessilfie09@gmail.com>
2020-10-19 00:33:26 -0500

    Add delete_class_year action

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -10,4 +10,12 @@ class UploadsController < ApplicationController
     flash[:success] = "#{records_added} New Records Imported!"
     redirect_to uploads_path
   end
+
+  def delete_class_year
+    class_year = params[:class_year]
+    Student.where(:class_year => class_year).destroy_all
+    flash[:success] = "You have removed all records for #{class_year}!"
+    redirect_to uploads_path
+  end
+
 end
