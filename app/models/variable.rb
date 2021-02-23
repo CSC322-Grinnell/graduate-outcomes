f41203e5f1ec293656a843058d1c761f016abd51
Ally-R <ally.rogers.mail@gmail.com>
2021-02-23 11:02:08 -0800

    documentation for validations

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,3 +1,13 @@
+## DOC OVERVIEW:
+# This document runs basic validations for Chart Variables for field. Other
+# validations can be found in other models, and general validations are in
+# visualization.rb
+
+## SPECIFIC VALIDATIONS:
+# * The variable belongs to a visualization
+# * That visualization exists
+# * If either name or role is filled out, then so is the other.
+
 class Variable < ApplicationRecord
   belongs_to :visualization
   validates :name, presence: true, if: -> {role.present?}
