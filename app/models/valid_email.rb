ec78665d24a63449489ca77b092f6b7719662de5
Erin Scherl <rinrun413@gmail.com>
2021-03-03 20:43:09 -0600

    wording changes

--- app/models/valid_email.rb
+++ app/models/valid_email.rb
@@ -1,4 +1,5 @@
 class ValidEmail < ApplicationRecord
+    before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
         format: { with: VALID_EMAIL_REGEX },
