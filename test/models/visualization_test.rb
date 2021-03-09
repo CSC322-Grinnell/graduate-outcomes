1ca12ea2157b97c56a278cae8740761b1cc4ddd3
Erin Scherl <rinrun413@gmail.com>
2021-03-08 18:13:49 -0600

    make tests pass

--- test/models/visualization_test.rb
+++ test/models/visualization_test.rb
@@ -6,6 +6,7 @@ class VisualizationTest < ActiveSupport::TestCase
 
   # Initialze two visualizations, one invalid and one valid
   def setup
+    @student = Student.create(class_year: "2018", student_id: "12345", major1: "BIO", major2: "CSC", gender: "M", fed_group: "white", intern: "TRUE", research: "FALSE", service: "TRUE", career_related: "moderately", job_field: "consulting", FDS_cat: "employ", gs_select: "TRUE", gs_level: "doctoral", gs_type: "science")
     @Vis1 = Visualization.new(chart_title: "Dummy", x_axis_title: "x-axis", y_axis_title: "y-axis", chart_type: "bar")
     @Vis2 = Visualization.new(chart_title: "Dummy", x_axis_title: "x-axis", y_axis_title: "y-axis", chart_type: "bar")
     @Vis2.variables.new(name: "dingleberry", role: "fun") # add a variable to Vis2 to make it valid
@@ -16,6 +17,11 @@ class VisualizationTest < ActiveSupport::TestCase
     assert_not @Vis1.valid?
   end
 
+  test "Vis2 should not be valid without any data" do
+    @student.destroy
+    assert_not @Vis2.valid?
+  end
+  
   # Vis2 should be valid
   test "Vis2 should be valid" do
     assert @Vis2.valid?
