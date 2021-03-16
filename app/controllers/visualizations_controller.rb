16b82bbcac20f930980f78579a1f5ec6ddd76ca7
Ally-R <ally.rogers.mail@gmail.com>
2021-03-16 00:18:10 -0700

    better documentation for dynamic form elements

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -1,3 +1,8 @@
+# Controller for all visualization form and rendering
+
+# Soruce for dynamic form fields:
+# https://rbudiharso.wordpress.com/2010/07/07/dynamically-add-and-remove-input-field-in-rails-without-javascript/
+
 class VisualizationsController < ApplicationController
   before_action :set_visualization, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user
@@ -32,20 +37,20 @@ class VisualizationsController < ApplicationController
 
   def create
     @visualization = Visualization.new(visualization_params)
-    if params[:add_variable]
+    if params[:add_variable] # if button selected to add var, re-render form
       @visualization.variables.build
       render 'new'
     elsif params[:remove_variable]
       render 'new'
-    elsif params[:add_filter]
-      # add empty ingredient associated with @recipe
+    elsif params[:add_filter] # if button selected to add filt, re-render form
+      # add empty filter associated with @visualization
       @visualization.filters.build
       render 'new'
     elsif params[:remove_filter]
-      # nested model that have _destroy attribute = 1 automatically deleted by rails
+      # nested models that have _destroy attribute = 1 automatically deleted by rails (except not working!!)
       render 'new'
     else
-      # save goes like usual
+      # save
       if @visualization.save
          flash[:success] = "Visualization created!"
          redirect_to @visualization #show visualization that was just created
@@ -58,52 +63,52 @@ class VisualizationsController < ApplicationController
   def update
     @visualization = Visualization.find(params[:id])
     if params[:add_variable]
-    	# rebuild the ingredient attributes that doesn't have an id
+    	# rebuild the variable attributes that don't have an id
     	unless params[:variable][:variables_attributes].blank?
 	  for attribute in params[:variable][:variables_attributes]
 	    @visualization.variables.build(attribute.last.except(:_destroy)) unless attribute.last.has_key?(:id)
 	  end
     	end
-      # add one more empty ingredient attribute
+      # add one more empty variable attribute
         @visualization.variables.build
-        render :action => 'edit'
+        render :action => 'edit' # re-render form
     elsif params[:remove_variable]
-      # collect all marked for delete ingredient ids
+      # collect all marked for delete variable ids
       removed_variables = params[:visualization][:variables_attributes].collect { |i, att| att[:id] if (att[:id] && att[:_destroy].to_i == 1) }
-      # physically delete the ingredients from database
+      # physically delete the variables from database (NOT WORKING!!)
       Visualization.delete(removed_variables)
       flash[:notice] = "Variables removed."
       for attribute in params[:visualization][:variables_attributes]
-      	# rebuild ingredients attributes that doesn't have an id and its _destroy attribute is not 1
+      	# rebuild variables attributes that don't have an id and whose _destroy attribute is not 1
         @visualization.variables.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
       end
       render :action => 'edit'
     elsif params[:add_filter]
-    	# rebuild the ingredient attributes that doesn't have an id
+    	# rebuild the filter attributes that don't have an id
     	unless params[:filter][:filters_attributes].blank?
 	  for attribute in params[:filter][:filters_attributes]
 	    @visualization.filters.build(attribute.last.except(:_destroy)) unless attribute.last.has_key?(:id)
 	  end
     	end
-      # add one more empty ingredient attribute
+      # add one more empty filter attribute
         @visualization.filters.build
         render :action => 'edit'
     elsif params[:remove_filter]
-      # collect all marked for delete ingredient ids
+      # collect all marked for delete filter ids
       removed_filters = params[:visualization][:filters_attributes].collect { |i, att| att[:id] if (att[:id] && att[:_destroy].to_i == 1) }
-      # physically delete the ingredients from database
+      # physically delete the filters from database (NOT WORKING!!)
       Visualization.delete(removed_filters)
       flash[:notice] = "Filters removed."
       for attribute in params[:visualization][:filters_attributes]
-      	# rebuild ingredients attributes that doesn't have an id and its _destroy attribute is not 1
+      	# rebuild filter attributes that dont't have an id and whose _destroy attribute is not 1
         @visualization.filters.build(attribute.last.except(:_destroy)) if (!attribute.last.has_key?(:id) && attribute.last[:_destroy].to_i == 0)
       end
-      render :action => 'edit'
+      render :action => 'edit' # re-render
     else
-      # save goes like usual
+      # save
       if @visualization.update(visualization_params)
         flash[:success] = "Visualization updated!"
-        redirect_to @visualization #show visualization that was just update
+        redirect_to @visualization #show visualization that was just updates
       else
         flash[:error] = "Visualization could not be updated. Please fill all fields and try again!"
         redirect_to visualizations_path # ideally stay on edit and show error messages but it appears to be building extra variables/filters
